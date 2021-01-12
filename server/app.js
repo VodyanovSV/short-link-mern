6b07a3185d747d856bf677fd0a91d0c36185ec78
Sergey Vodyanov <VodyanovSV@gmail.com>
2021-01-12 18:16:04 +0300

    create express server

--- server/app.js
+++ server/app.js
@@ -1,2 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+const PORT = config.get('port') || 5000
+
+app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+
+
+
 
 
