3d15a2002a3b01937b567be40890d24ef6e90b57
Sergey Vodyanov <VodyanovSV@gmail.com>
2021-01-15 18:34:03 +0300

    connected to the mongodb

--- server/app.js
+++ server/app.js
@@ -1,10 +1,25 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import config from 'config'
 
 const app = express()
 
 const PORT = config.get('port') || 5000
 
-app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        })
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+    } catch (e) {
+        console.log('Server Error', e.message)
+        process.exit(1)
+    }
+}
+
+start()
 
 
 
