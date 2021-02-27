c4107e9e0f417efcf5f1c13b8f1c5c0b7069e387
Sergey Vodyanov <VodyanovSV@gmail.com>
2021-02-27 17:40:15 +0300

    create context and add login handler on client

--- client/src/pages/auth-page/AuthPage.js
+++ client/src/pages/auth-page/AuthPage.js
@@ -2,10 +2,10 @@ import React from 'react'
 import styles from './AuthPage.module.scss'
 import {useHttp} from "../../hooks/http.hook";
 import {useMessage} from "../../hooks/message.hook";
-
+import {AuthContext} from "../../context/AuthContext";
 
 export const AuthPage = () => {
-	
+	const auth = useContext(AuthContext)
 	const message = useMessage()
 	const {loading, request, error, clearError} = useHttp()
 	const [form, setForm] = useState({email: '', password: ''})
@@ -30,7 +30,15 @@ export const AuthPage = () => {
 
         }
     }
-	
+
+    const loginHandler = async () => {
+        try {
+            const data = await request('/api/auth/login', 'POST', {...form})
+            auth.login(data.token, data.userId)
+        } catch (e) {
+
+        }
+    }	
 	
 	return (
         <div className='row'>
