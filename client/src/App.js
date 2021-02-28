c242a824597d93835b50c075dc89cbaacd4fadcd
Sergey Vodyanov <VodyanovSV@gmail.com>
2021-02-28 17:52:53 +0300

    create navbar on client

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,25 @@ import React from 'react'
 import {BrowserRouter as Router} from 'react-router-dom'
 import {useRoutes} from './hooks/routes.hook'
 import {useAuth} from "./hooks/auth.hook";
+import {AuthContext} from "./context/AuthContext";
+import {Navbar} from "./components/Navbar/Navbar";
 import './App.css'
 import 'materialize-css'
 
 function App() {
 	const {login, logout, token, userId, ready} = useAuth()
-	
-	const routes = useRoutes(false)
+	const isAuthenticated = !!token
+	const routes = useRoutes(isAuthenticated)
 
     return (
+		<AuthContext.Provider value={{login, logout, token, userId, isAuthenticated}}>	
             <Router>
+				{isAuthenticated && <Navbar/>}
                 <div className="container">
                     {routes}
                 </div>
             </Router>
+        </AuthContext.Provider>
     );
 }
 
