713fb7374efc6cae8ad3f332668df4c3c34b94fe
Sergey Vodyanov <VodyanovSV@gmail.com>
2021-03-13 18:18:40 +0300

    create link controller generate on server

--- server/controllers/link.controllers.js
+++ server/controllers/link.controllers.js
@@ -1,9 +1,27 @@
 import Link from '../models/Link.js'
+import shortid from 'shortid'
+import config from 'config'
 
 class LinkControllers {
     async generate(req, res) {
         try {
+			const baseUrl = config.get('baseUrl')
+            const {from} = req.body
 
+            const code = shortid.generate()
+
+            const existing = await Link.findOne({from})
+
+            if (existing) {
+                return res.json({link: existing})
+            }
+
+            const to = baseUrl + '/t/' + code
+            const link = new Link({
+                from, to, code, owner: req.user.userID
+            })
+            await link.save()
+            res.status(201).json({link})
         } catch (e) {
             res.status(500).json({message: "Что-то пошло не так, попробуйте снова"})
         }
